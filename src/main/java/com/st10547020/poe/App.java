45996aaa8b04272767b2b860cc362a74901f5a42
ST10547020 <ST10547020@rcconnect.edu.za>
2026-09-15 16:34:47 +0200

    Setup Maven project with JUnit 5 configuration

---
+++ src/main/java/com/st10547020/poe/App.java
@@ -0,0 +1,13 @@
+package com.st10547020.poe;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/com/st10547020/poe/AppTest.java
@@ -0,0 +1,11 @@
+package com.st10547020.poe;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class AppTest {
+    @Test
+    public void placeholderTest() {
+        assertTrue(true);
+    }
+}
